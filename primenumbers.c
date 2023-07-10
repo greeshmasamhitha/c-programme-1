c3f7bd5cbc11ce28603050e0f450cdb5cf3fdd1c
greeshmasamhitha <greeshmasreenivasulu@gmail.com>
2023-07-10 11:24:07 +0530

    M

---
+++ primenumbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int i,j,n,fact;
+	system("clear");
+	printf("\n enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact=0;
+		for(j=1;j<=n;j++)
+		{
+			if(i%j==0)
+				fact++;
+		}
+		if(fact==2)
+		
+			printf("%d\t\n",i);
+		
+	}
+}
+
+
+
