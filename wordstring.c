b711a041586600d3dcb7700813c7d4db300adc5f
greeshmasamhitha <greeshmasreenivasulu@gmail.com>
2023-07-22 18:49:29 +0530

    m

---
+++ wordstring.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+  char string[50]="GREESHMA SAMHITHA" ;
+        system("clear");
+        printf("%s\n\n\n",strchr(string,'H'));
+        
+}
+
